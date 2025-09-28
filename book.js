f32be3d9abe9acd576b3993d99ca09b7dcd3ac7b
小朱 <zbx2007420@qq.com>
2025-09-28 19:18:38 +0800

    one add

---
+++ book.js
@@ -0,0 +1,28 @@
+module.exports = {
+    plugins: [
+      "-lunr",
+      "-search",
+      "search-pro",
+      "code",
+      "theme-lou",
+      "expandable-chapters",
+      "back-to-top-button"
+    ],
+    pluginsConfig: {
+      // "search-pro": {
+      //   "path": "search_plus_index.json"
+      // },
+      // "code": {
+      //   "theme": "github"
+      // }
+    },
+  
+    structure: {
+      readme: "README.md",
+      summary: "SUMMARY.md"
+    },
+  
+    styles: {
+      website: "styles/website.css"
+    }
+  };
